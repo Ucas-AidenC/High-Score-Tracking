f231e7fe974a211852bfeae98af61e30058b6271
Ucas-AidenC <aiden.challenger@ucas-edu.net>
2025-02-25 10:51:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 #main.py initial commit 
+"""
+r= Read(will give error if no file exists)
+W=write (will give error if no file exists) 
+W+= read and write (will give error if no file exists) 
+A = append(add thing that cannot be overwritten)(will create a file if one doesn’t exist) 
+A+ = read and append 
+X = create a file 
+"""
+
+#use varible for file path string to change what score isa printed
+
+
+def save_score(parameter depending on which game):
+    with open ("fille path thing/.csv", "w" ) as file:
+        for rows in collmns#fix name 
+
+
+
+def load_score():
+
+
+
+
+def user_highscore():
+    with open ("fille path thing/.csv", "w" ) as file:\
+    #for loop 
+    # pritn thig \
+
+
+#comparing scores 
+if score >= highscore
+score = high score 
+with open ("fille path thing/.csv", "w" ) as file:
+    #read info then add to profile name 
+    file.writer(gighscore )
+
